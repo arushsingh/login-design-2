6f37ea1de2f8ef5ddc2316c86a90f24477f45256
arush@snakebite.com <arush@snakebite.com>
2018-06-03 18:22:06 +0530

    login-form

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Demo or didn't happen
 
 var signin = document.querySelector("#signin");
 var register = document.querySelector("#register");
 setTimeout(function () {
     register.checked = true;
-}, 1000);
+}, 0);
 setTimeout(function () {
     signin.checked = true;
-}, 2000);
+}, 0);
